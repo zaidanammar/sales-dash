91788014d5fe053292ef9701ed27f936f1a1b3c2
zaidanammar <zaidanammar23@gmail.com>
2022-11-15 18:53:06 +0700

    :zap: add profile component

--- src/components/templates/rightbar/index.tsx
+++ src/components/templates/rightbar/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Profile from '../../molecules/profile'
 
 const TemplatesRightBar = () => (
-  <div className="w-[408px] bg-background p-8 lg:block hidden">
-    TemplatesRightBar
+  <div className="w-[408px] bg-secondary p-8 lg:block hidden">
+    <Profile />
   </div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,11 @@ module.exports = {
       colors: {
         primary: '#545DFF',
         background: '#FCFCFF',
-        secondary: '#D2D2D2',
+        secondary: '#F3F4FF',
         tertiary: '#FFE7BD',
         textPrimary: '#727272',
-        textSecondary: '#707070C4',
+        textDark: '#212121',
+        textDark2: '#7A7A7A',
       },
       fontFamily: {
         sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
